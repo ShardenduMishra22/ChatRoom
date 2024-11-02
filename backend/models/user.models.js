8a8cc9056d3e0dc3f1433aef2985680d32d041ab
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-11-02 13:24:47 +0530

    Done

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -2,35 +2,34 @@ import mongoose from "mongoose";
 import pkg from "bcryptjs";
 const { hashSync } = pkg;
 
-// Function to generate a random avatar URL
 const generateAvatarUrl = (seed) => {
     return `https://api.dicebear.com/9.x/lorelei/svg?seed=${seed}`;
 };
 
 const userSchema = mongoose.Schema(
     {
-        name: {
-            type: String,
-            required: true
+        name : {
+            type : String,
+            required : true
         },
-        email: {
-            type: String,
-            required: true,
-            unique: true // Ensure email uniqueness
+        email : {
+            type : String,
+            required : true,
+            unique : true
         },
-        password: {
-            type: String,
-            required: true
+        password : {
+            type : String,
+            required : true
         },
-        avatar: {
-            type: String,
-            default: function() {
+        avatar : {
+            type : String,
+            default : function() {
                 return generateAvatarUrl(this.email);
             }
         },
     },
     {
-        timestamps: true
+        timestamps : true
     }
 );
 
